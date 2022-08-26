480765839c65ef4c21d46a51e386e1a49b2f68f3
suhyeon <kim113355@naver.com>
2022-08-26 17:34:31 +0900

    ✨ 상품리스트 필터 및 정렬 적용시 쿼리스트링 변경

--- src/modules/actions/devicesSlice.js
+++ src/modules/actions/devicesSlice.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { FILTER_DATA } from '../../components/device/DeviceListFileterContents';
+
+const ALL = 'all';
+
 const initialState = {
   devices: [
     {
@@ -63,12 +67,17 @@ const initialState = {
     plan: null,
     discount: null,
     price: [],
-    company: new Set(),
-    storage: new Set(),
+    company: [],
+    storage: [],
   },
   options: {
+    search: null,
     soldout: false,
     origin_price: false,
+    sort: {
+      direction: true,
+      by: 'launch',
+    },
   },
 };
 
@@ -76,9 +85,6 @@ const devicesSlice = createSlice({
   name: 'devices',
   initialState,
   reducers: {
-    getDevices: (state, action) => {
-      console.log('get devices');
-    },
     getDevicesSuccess: (state, action) => {
       console.log('get devices success!');
     },
@@ -87,9 +93,41 @@ const devicesSlice = createSlice({
     },
     setFilterValue: (state, action) => {
       const { type, value } = action.payload;
-      if (type === 'plan' || type === 'discount') {
+      const type_name = `${type}_type`; // PLNA_TYPE, DISCOUNT_TYPE, COMPANY_TYPE, STORAGE_TYPE
+      if (type === FILTER_DATA.plan_type.name || type === FILTER_DATA.discount_type.name) {
         state.filter[type] = value;
-      } else if (type === 'company' || type === 'storage') {
+      } else if (type === FILTER_DATA.company_type.name || type === FILTER_DATA.storage_type.name) {
+        // 없으면 add, 이미 존재하면 delete
+        // 전체(all)의 경우 존재하지만 선택했다면 변화없음
+        // 전체(all) 이외에 모든 checkbox가 선택되었다면 all만 add
+        // 전체(all) 이외에 모든 checkbox가 미선택되었다면 all만 add
+        let exist = state.filter[type]; // 기존에 담긴 datas
+        const isChecked = exist.includes(value); // 이미 선택된 값인지 확인
+        if (isChecked && value !== ALL) {
+          // 이미 선택되었는데, 전체를 선택한 경우라면 변화없음
+          exist = exist.filter((v) => v !== value);
+          // 지웠는데 모든 값이 지워졌다면 all 추가
+          if (!exist.length) exist.push(ALL);
+        }
+
+        if (!isChecked) {
+          if (value === ALL) {
+            // 전체를 선택했다면 모든 값 지우고 all add
+            exist = [ALL];
+          } else {
+            exist = exist.filter((v) => v !== ALL);
+            exist.push(value);
+            if (exist.length === FILTER_DATA[type_name].data.length - 1) {
+              // 전체 이외 모든 체크박스가 선택되었다면 all add
+              exist.splice(0, exist.length);
+              exist.push(ALL);
+            }
+          }
+        }
+        state.filter[type] = exist;
+      } else if (type === FILTER_DATA.price_range.name) {
+        // 가격 범위
+        state.filter[type] = value;
       }
     },
   },
